eee9738d1a1445ed3c842b2070a182597bec2253
Kamsi-yonna <etiobikamsi@gmail.com>
2024-02-06 17:25:31 +0100

    feat - footer  feature

---
+++ components/partials/FooterLinks.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <section>
+      <ul>
+        <li>About</li>
+        <li>Company</li>
+        <li>Locations</li>
+        <li>Contact</li>
+        <li>Hours</li>
+      </ul>
+      <ul>
+        <li>Twitter</li>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>LinkedIn</li>
+      </ul>
+      <ul>
+        <h4>Newsletter:</h4>
+        <input type="text" />
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+section {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 80%;
+  margin-left: 10%;
+  padding: 40px 0;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+
+  li {
+    cursor: pointer;
+
+    width: auto;
+  }
+  :hover {
+    color: #c1403c;
+  }
+}
+
+input[type="text"] {
+  width: 100%;
+  color: black;
+  box-sizing: border-box;
+  padding: 10px 10px;
+  border: 1px solid #000;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+}
+
+input[type="text"]:focus {
+  border: none;
+  color: black;
+  border: 1px solid #000;
+}
+
+@media screen and (min-width: 850px) {
+  section {
+    padding: 40px !important;
+  }
+}
+</style>
---
+++ components/partials/FooterNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer>
+    <section class="callout">
+      <h2>
+        "The surprising styles of Skyline Ivy are advanced for all seasons."
+      </h2>
+      <p>Hansel Andersen</p>
+    </section>
+
+    <FooterLinks />
+
+    <div class="footer-bottom">
+      By
+      <a href="http://github.com/Kamsi-yonna" target="_blank">Kamsiyonna</a>
+      &copy; {{ currentYear }}
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
+
+<style scoped>
+.callout {
+  border: 1px solid black;
+  display: flex;
+  height: 280px;
+
+  width: 100%;
+  background: url("/public/assets/images/callout.jpg") center center no-repeat;
+  background-size: cover;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+}
+
+.callout h2 {
+  padding: 0 30px;
+  text-align: center;
+}
+
+.footer-bottom {
+  text-align: center;
+
+  a {
+    color: #fff;
+  }
+  :hover {
+    color: #c14103;
+    text-decoration: underline;
+  }
+}
+</style>
